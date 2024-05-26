ad9d983d9affda178fdf48a526119e78ce4ac432
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-05-26 10:14:07 +0530

    added cards in activity screen working fine only bg-image

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,10 +2,24 @@ import React from "react";
 import './index.css'
 
 function Card({card, onClick}) {
+
+    const IsEven = ({card}) => {
+        if(card.isEven === false) {
+            return (
+                <img src={require('..\\public\\pink-card-bg.png')} alt="pink-card"/>
+            );
+        } else {
+            return (
+                <img src={require('..\\public\\blue-card-bg.png')} alt="blue-card" />
+            );
+        }
+    }
+
     return (
         <div className="card" onClick={onClick}>
             <div className="card-content">
-                {card.flipped || card.matched ? card.value: '?'}
+                {card.flipped || card.matched ? card.value: <IsEven card={card} key={card.key} />}
+                {/* {<IsEven card={card} key={card.key} />} */}
             </div>
         </div>
     )
