f0d2a7f4c68427bf19c1e196507f62e62dd1fa2b
kungumasakthivel <kungumasakthivel.g2020aids@sece.ac.in>
2024-05-26 11:05:34 +0530

    added alphabets open card working fine

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -15,11 +15,40 @@ function Card({card, onClick}) {
         }
     }
 
+    const OpenCard = ({card}) => {
+        if(card.value === 'A') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-a.jpg')} alt="card-a" />
+            );
+        } else if(card.value === 'B') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-b.jpg')} alt="card-b" />
+            );
+        } else if(card.value === 'C') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-c.jpg')} alt="card-c" />
+            );
+        } else if(card.value === 'D') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-d.jpg')} alt="card-d" />
+            );
+        } else if(card.value === 'E') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-e.jpg')} alt="card-e" />
+            );
+        } else if(card.value === 'F') {
+            return (
+                <img className="open-card" src={require('..\\public\\card-f.jpg')} alt="card-f" />
+            );
+        }
+    }
+
     return (
         <div className="card" onClick={onClick}>
             <div className="card-content">
-                {card.flipped || card.matched ? card.value: <IsEven card={card} key={card.key} />}
-                {/* {<IsEven card={card} key={card.key} />} */}
+                {/* {card.flipped || card.matched ? card.value: <IsEven card={card} key={card.key} />} */}
+                {card.flipped || card.matched ? <OpenCard card={card} key={card.id} />: <IsEven card={card} key={card.id} />}
+
             </div>
         </div>
     )
